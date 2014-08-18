53db6b15bcee266b8cd63886177a36cb77153c19
Fernando Almeida <fernando.almeida@me.com>
2014-08-18 17:05:43 -0300

    Update terminal-resolution.sh

--- terminal-resolution.sh
+++ terminal-resolution.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-GRUBCFG=/etc/default/grub
+VERSION=1.0
+GRUBCFG="/etc/default/grub"
 GRUBBKP="/var/tmp/grub $(date +'%m-%d-%Y %H%M%S')"
 
 function isroot {
@@ -17,19 +18,19 @@ if [ -z $1 ]; then
 fi
 
 if [ ! -f $GRUBCFG ]; then
-        echo File $GRUBCFG not found. GRUB2 is really installed?
+        echo "File $GRUBCFG not found. GRUB2 is really installed?"
         exit
 fi
 
 if [ $1 == "--help" ]; then
         echo "Set TERMINAL and GRUB2 resolution."
         echo " "
-        echo terminal-resolution.sh [width_resxheight_res] [-yn]
+        echo "terminal-resolution.sh [width_resxheight_res] [-yn]"
         echo " "
         echo "-y\tAnswer Y to reboot question"
         echo "-n\tNo reboot"
         echo " "
-        echo NOTE: You must be root in order to run it!
+        echo "NOTE: You must be root in order to run it!"
         echo " "
         exit
 fi
@@ -42,8 +43,8 @@ if [ isroot ]; then
         sed -i 's/#GRUB_GFXMODE/GRUB_GFXMODE/' "$GRUBCFG"
         sed -i 's/^\(GRUB_GFXMODE=\).*/\1'$1'/' "$GRUBCFG"
 
-        if [ $(grep -ci "GRUB_GFXPAYLOAD_LINUX" $GRUBCFG) ]; then
-                echo "GRUB_GFXPAYLOAD_LINUX=keep" >> $GRUBCFG
+        if [ $(grep -ci "GRUB_GFXPAYLOAD_LINUX" "$GRUBCFG") ]; then
+                echo "GRUB_GFXPAYLOAD_LINUX=keep" >> "$GRUBCFG"
         else
                 sed -i 's/^\(GRUB_GFXPAYLOAD_LINUX=\).*/\1keep/' "$GRUBCFG"
         fi
@@ -61,6 +62,6 @@ if [ isroot ]; then
                 fi
         fi
 else
-        echo Error: You must be root in order to run
+        echo "Error: You must be root in order to run"
         exit
 fi
